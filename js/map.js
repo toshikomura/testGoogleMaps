4623f5378851fd9f936e1ea1ff47ae1e064dcd3a
Gustavo Toshi Komura <gtk10@c3sl.ufpr.br>
2013-08-20 16:34:04 -0700

    Include variable to option of the map
    Signed-off-by: Gustavo Toshi Komura <gtk10@c3sl.ufpr.br>

--- js/map.js
+++ js/map.js
@@ -1,2 +1,8 @@
 var mapDiv = document.getElementById('map');
 var latlng = new google.maps.LatLng(37.09, -95.71);
+
+var options = {
+  center: latlng,
+  zoom: 4,
+  mapTypeId: google.maps.MapTypeId.SATELLITE
+};
