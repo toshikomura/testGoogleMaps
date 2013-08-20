9e886bc74f902bd1be07f7619c3a026b7e8796b0
Gustavo Toshi Komura <gtk10@c3sl.ufpr.br>
2013-08-20 16:40:19 -0700

    Include function to execute after read page
    Signed-off-by: Gustavo Toshi Komura <gtk10@c3sl.ufpr.br>

--- js/map.js
+++ js/map.js
@@ -1,10 +1,12 @@
-var mapDiv = document.getElementById('map');
-var latlng = new google.maps.LatLng(37.09, -95.71);
+window.onload = function() {
+  var mapDiv = document.getElementById('map');
+  var latlng = new google.maps.LatLng(37.09, -95.71);
 
-var options = {
-  center: latlng,
-  zoom: 4,
-  mapTypeId: google.maps.MapTypeId.SATELLITE
-};
+  var options = {
+    center: latlng,
+    zoom: 4,
+    mapTypeId: google.maps.MapTypeId.SATELLITE
+  };
 
-var map = new google.maps.Map(mapDiv, options);
+  var map = new google.maps.Map(mapDiv, options);
+}
