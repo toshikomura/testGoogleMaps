9f3d539536c74096b2c75a83aff632e5f3e67a99
Gustavo Toshi Komura <gtk10@c3sl.ufpr.br>
2013-08-20 17:09:23 -0700

    Encapsulate function to create map
    Signed-off-by: Gustavo Toshi Komura <gtk10@c3sl.ufpr.br>

--- js/map.js
+++ js/map.js
@@ -1,12 +1,14 @@
-window.onload = function() {
-  var mapDiv = document.getElementById('map');
-  var latlng = new google.maps.LatLng(-25.5350, -49.2058);
+(function() {
+  window.onload = function() {
+    var mapDiv = document.getElementById('map');
+    var latlng = new google.maps.LatLng(-25.5350, -49.2058);
 
-  var options = {
-    center: latlng,
-    zoom: 15,
-    mapTypeId: google.maps.MapTypeId.SATELLITE
-  };
+    var options = {
+      center: latlng,
+      zoom: 15,
+      mapTypeId: google.maps.MapTypeId.SATELLITE
+    };
 
-  var map = new google.maps.Map(mapDiv, options);
-}
+    var map = new google.maps.Map(mapDiv, options);
+ }
+})();
