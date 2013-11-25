1498577eb0f6c4d8a3478df9984dc90a85035962
Gustavo Toshi Komura <gtk10@c3sl.ufpr.br>
2013-11-25 12:10:04 -0200

    Include new test app and add "geocoder" gem
    Signed-off-by: Gustavo Toshi Komura <gtk10@c3sl.ufpr.br>

---
+++ mapas/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mapas::Application.config.secret_token = '8cef73db3d26cc51f8d76dc853e0abe95314eecdec323c8a659b88b5e6b5bd7712db5456eef5e5fb0d36aae14e9504b76047daa0b97065c2e8885ae5b7a6eb5e'
