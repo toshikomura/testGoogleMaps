5abf44dcfd5a02c033822490a2372f2f7fb72628
Gustavo Toshi Komura <gtk10@c3sl.ufpr.br>
2013-11-25 14:09:07 -0200

    Add basic configuration geocoder by "RailsCast"
    Signed-off-by: Gustavo Toshi Komura <gtk10@c3sl.ufpr.br>

--- mapas/app/controllers/locations_controller.rb
+++ mapas/app/controllers/locations_controller.rb
@@ -1,6 +1,11 @@
 class LocationsController < ApplicationController
+
   def index
-    @locations = Location.all
+    if params[:search].present?
+      @locations = Location.near(params[:search], 50, :order => :distance)
+    else
+      @locations = Location.all
+    end
   end
 
   def show
